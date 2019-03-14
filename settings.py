506d97bc4f62fe2dedc57c76db1337882988321e
Rvbens <ruben_onelove@hotmail.com>
2019-03-14 18:52:25 +0100

    feat:  Add bot scripts
    Script to load the model and feed the line one by one and integration with Slack.

--- settings.py
+++ settings.py
@@ -6,11 +6,11 @@ EOS_token = 2  # End-of-sentence token
 UNK_token = 3
 
 MAX_LENGTH = 20        # Tamaño de las frases
-LINES_USED = 2_500_000 # Determina el tamaño de la parte de dataset que cogemos
+LINES_USED = 500_000 # Determina el tamaño de la parte de dataset que cogemos
 MIN_COUNT  = 10         # Minimum word count threshold for trimming
 WITH_UNK   = True      # Swap words not in voc for unk token, instead of deleting sentence
 
 corpus = "./data/es.txt"
-corpus_name = "OpenSubtitle_PUNCT3"
+corpus_name = "OpenSubtitle_P3_500k"
 if WITH_UNK: corpus_name += "_UNK"
 save_dir = os.path.join("data", corpus_name)
